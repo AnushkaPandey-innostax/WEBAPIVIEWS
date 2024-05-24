dadabee58efc5d61bb4863b2a1d9376d87858fdf
AnushkaPandey-innostax <anushka.pandey@innostax.com>
2024-05-24 19:30:59 +0530

    Add project files.

---
+++ WEBAPIVIEWS/Controllers/BrandController.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Security.Policy;
+using System.Text;
+using WEBAPIVIEWS.Models;
+
+namespace WEBAPIVIEWS.Controllers
+{
+    public class BrandController : Controller
+    {
+
+            private string url = "http://localhost:5136/api/brands/";
+            private HttpClient client = new HttpClient();
+            [HttpGet]
+            public IActionResult Index()
+            {
+                List<Brand> brands = new List<Brand>();
+                HttpResponseMessage response = client.GetAsync(url).Result;
+                if (response.IsSuccessStatusCode)
+                {
+                    string result = response.Content.ReadAsStringAsync().Result;
+                    var data = JsonConvert.DeserializeObject<List<Brand>>(result);
+                    if (data != null)
+                    {
+                        brands = data;
+                }
+                }
+                return View(brands);
+            }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();  
+        }
+        [HttpPost]
+        public IActionResult Create(Brand brands)
+        {
+            String data = JsonConvert.SerializeObject(brands);
+            StringContent content = new StringContent(data,Encoding.UTF8,"application/json");
+            HttpResponseMessage response =client.PostAsync(url, content).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                TempData["insert_message"] = "Brand Added...";
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+    
+    [HttpGet]
+    public IActionResult Edit(int id)
+    {
+        Brand bd = new Brand();
+        HttpResponseMessage response = client.GetAsync(url + id).Result;
+            if(response.IsSuccessStatusCode)
+            {
+                string result = response.Content.ReadAsStringAsync().Result;
+                var data = JsonConvert.DeserializeObject<Brand>(result);
+                if (data != null)
+                {
+                    bd = data;
+                }
+            }
+        return View();
+    }
+        [HttpPut]
+        public IActionResult Edit(Brand brands)
+        {
+            String data = JsonConvert.SerializeObject(brands);
+            StringContent content = new StringContent(data, Encoding.UTF8, "application/json");
+            HttpResponseMessage response = client.PutAsync(url + brands.id, content).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                TempData["update_message"] = "Brand Updated...";
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+    }
+
+}
---
+++ WEBAPIVIEWS/Models/Brand.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WEBAPIVIEWS.Models
+{
+    public class Brand
+    {
+     
+            public int id { get; set; }
+        [Required]
+            public string name { get; set; }
+        [Required]
+        public string category { get; set; }
+        [Required]
+        public int isActive { get; set; }
+
+        }
+    }
+
